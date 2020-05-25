31a97b4dba9d9dbd1d01976d7969052d57863269
DevanshR1123 <65907921+DevanshR1123@users.noreply.github.com>
2020-05-25 20:43:43 +0530

    Update mapplot.py

--- mapplot.py
+++ mapplot.py
@@ -1,6 +1,5 @@
 import folium as fl
 from data import hotels, places
-from functools import reduce
 from pandas import DataFrame as df
 # 18.89,19.21,19.05
 # 72.77,72.95,72.86
@@ -11,7 +10,7 @@ hlocs = list(map(lambda p: (p['lat'], p['lng']), hotels))
 hnames = list(map(lambda p: p['name'], hotels))
 
 
-def map_path(start, path, time, file_path):
+def map_path(start, path, time):
     mp = fl.Map(location=[19.05, 72.86], zoom_start=16)
     mp.fit_bounds([(18.90, 72.77), (19.16, 72.95)])
     loc_path = list(map(lambda i: plocs[i], path))
@@ -23,7 +22,6 @@ def map_path(start, path, time, file_path):
         fl.Marker(location=l, tooltip=str(i+2)+'. '+n, icon=fl.Icon(
             color='red', icon_color='lightgray')).add_to(mp)
 
-    # reduce(lambda a, x: a + f'<li> {x[0]}', name_path, '')
     plist = df(zip(name_path, map(lambda x: f'{int(x//1)} hrs {int((x%1)*60)} mins', time)),
                columns=['Location', 'Time spent'], index=range(1, len(path))).to_html(border=0, classes='tbl')
 
@@ -46,4 +44,4 @@ def map_path(start, path, time, file_path):
         </div></div>'''
     mp.get_root().html.add_child(fl.Element(legend))
 
-    mp.save(file_path)
+    mp.save('./map.html')

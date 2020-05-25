16871029b77a7f90f41b7eedb57bf128203b5615
DevanshR1123 <65907921+DevanshR1123@users.noreply.github.com>
2020-05-25 21:06:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from data import hotels, places
 from mapplot import map_path
 from random import randint,random
 
-import os
-print(os.getcwd())
+open('test.txt','w+').write('Test 1, 2, 3')
 
 nhotels = len(hotels)
 nplaces = len(places)

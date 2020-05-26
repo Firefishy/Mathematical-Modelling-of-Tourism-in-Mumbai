f641f9fd31e45609925be7c917318718a3b80371
DevanshR1123 <65907921+DevanshR1123@users.noreply.github.com>
2020-05-26 17:46:47 +0530

    Update mapplot.py

--- mapplot.py
+++ mapplot.py
@@ -39,8 +39,7 @@ def map_path(start, path, time):
         ">
         <style> 
         .tbl td,th {{padding: 0 .5em}}
-        /*.tblC {{font-size: 1em}}*/
-        @media (max-width:600px){{ .tblC {{font-size: 1em}} }}
+        @media (max-width:600px){{ .tblC {{font-size: .5em}} }}
         </style>
         <div class="tblC">
             <span style="margin-left: .5em"><b>Hotel:</b> {hnames[start]} </span>
